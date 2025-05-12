f85b8a2feebcfdb739a01ca0943eed4be48b2a37
blackSheep777 <forough.fallahfar@gmail.com>
2025-05-12 23:04:59 +0300

    chore(clean-up): remove unused files and add theme palette & fonts & fav icon

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="w-full h-[124px] p-2 bg-dodger_blue text-white">
+      <h1 className="text-3xl font-bold font-heading">SkySearch</h1>
+    </div>
+  )
+}
+
+export default Header
